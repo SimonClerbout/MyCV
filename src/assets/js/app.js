62216cae8e2717bb7c31f2498168c90663eee83f
Simon Clerbout <simon.clerbout@laposte.net>
2017-03-31 12:46:29 +0200

    add opengraph

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,11 +3,6 @@ $(document).foundation();
 $(document).ready(function() {
    $('#contact_form').bootstrapValidator({
        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
-       feedbackIcons: {
-           valid: 'glyphicon glyphicon-ok',
-           invalid: 'glyphicon glyphicon-remove',
-           validating: 'glyphicon glyphicon-refresh'
-       },
        fields: {
            first_name: {
                validators: {
@@ -39,55 +34,7 @@ $(document).ready(function() {
                    }
                }
            },
-           phone: {
-               validators: {
-                   notEmpty: {
-                       message: 'Please supply your phone number'
-                   },
-                   phone: {
-                       country: 'US',
-                       message: 'Please supply a vaild phone number with area code'
-                   }
-               }
-           },
-           address: {
-               validators: {
-                    stringLength: {
-                       min: 8,
-                   },
-                   notEmpty: {
-                       message: 'Please supply your street address'
-                   }
-               }
-           },
-           city: {
-               validators: {
-                    stringLength: {
-                       min: 4,
-                   },
-                   notEmpty: {
-                       message: 'Please supply your city'
-                   }
-               }
-           },
-           state: {
-               validators: {
-                   notEmpty: {
-                       message: 'Please select your state'
-                   }
-               }
-           },
-           zip: {
-               validators: {
-                   notEmpty: {
-                       message: 'Please supply your zip code'
-                   },
-                   zipCode: {
-                       country: 'US',
-                       message: 'Please supply a vaild zip code'
-                   }
-               }
-           },
+
            comment: {
                validators: {
                      stringLength: {
@@ -114,10 +61,5 @@ $(document).ready(function() {
 
            // Get the BootstrapValidator instance
            var bv = $form.data('bootstrapValidator');
-
-           // Use Ajax to submit form data
-           $.post($form.attr('action'), $form.serialize(), function(result) {
-               console.log(result);
-           }, 'json');
        });
 });
