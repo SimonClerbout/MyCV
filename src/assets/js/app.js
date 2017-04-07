6f7b4c806b83fa26044e42fb364b9fcd808c633c
Simon Clerbout <simon.clerbout@laposte.net>
2017-04-07 20:42:10 +0200

    add content

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,64 +1,61 @@
 $(document).foundation();
 
 $(document).ready(function() {
-   $('#contact_form').validate({
-       fields: {
-           first_name: {
-               validators: {
-                       stringLength: {
-                       min: 2,
-                   },
-                       notEmpty: {
-                       message: 'Please supply your first name'
-                   }
-               }
-           },
-            last_name: {
-               validators: {
-                    stringLength: {
-                       min: 2,
-                   },
-                   notEmpty: {
-                       message: 'Please supply your last name'
-                   }
-               }
-           },
-           email: {
-               validators: {
-                   notEmpty: {
-                       message: 'Please supply your email address'
-                   },
-                   emailAddress: {
-                       message: 'Please supply a valid email address'
-                   }
-               }
-           },
 
-           comment: {
-               validators: {
-                     stringLength: {
-                       min: 10,
-                       max: 200,
-                       message:'Please enter at least 10 characters and no more than 200'
-                   },
-                   notEmpty: {
-                       message: 'Please supply a description of your project'
-                   }
-                   }
-               }
-           }
-       })
-       .on('success.form.bv', function(e) {
-           $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
-               $('#contact_form').data('validate').resetForm();
+        $(".lw, .lo, .la, .lc, .lg, .ln, .lp, .lco, .le").hide();
 
-           // Prevent form submission
-           e.preventDefault();
+        $(".web").hover(function(){
 
-           // Get the form instance
-           var $form = $(e.target);
+         $('.lw').toggle();
 
-           // Get the BootstrapValidator instance
-           var bv = $form.data('validate');
        });
+
+       $(".oop").hover(function(){
+
+        $('.lo').toggle();
+
+      });
+
+      $(".android").hover(function(){
+
+       $('.la').toggle();
+
+     });
+
+     $(".c").hover(function(){
+
+      $('.lc').toggle();
+
+    });
+
+    $(".git").hover(function(){
+
+     $('.lg').toggle();
+
+   });
+
+   $(".network").hover(function(){
+
+    $('.ln').toggle();
+
+  });
+
+  $(".project").hover(function(){
+
+   $('.lp').toggle();
+
+ });
+
+ $(".comm").hover(function(){
+
+  $('.lco').toggle();
+
+});
+
+  $(".en").hover(function(){
+
+    $('.le').toggle();
+
+  });
+
 });
